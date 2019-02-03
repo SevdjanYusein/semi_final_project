8757f5124e0157e7cf8273a4bbdfa7589f6c31a3
AlexVaglarov <axell92@abv.bg>
2019-02-04 00:09:27 +0200

    blog and forum under construction

--- assets/scripts/select-change-page.js
+++ assets/scripts/select-change-page.js
@@ -6,6 +6,8 @@ window.addEventListener('DOMContentLoaded', function () {
        document.getElementById("containers").style.display="none";
        document.getElementById("poll").style.display="none";
        document.getElementById("ads").style.display="none";
+       document.getElementById("forum-uc").style.display="none";
+       document.getElementById("blog-uc").style.display="none";
        document.getElementById("upload-conteiner").style.display="block";
        document.getElementById("upload-conteiner").style.marginTop="20px";
     }, false);
@@ -16,6 +18,30 @@ window.addEventListener('DOMContentLoaded', function () {
        document.getElementById("containers").style.display="block";
        document.getElementById("poll").style.display="block";
        document.getElementById("upload-conteiner").style.display="none";
+       document.getElementById("forum-uc").style.display="none";
+       document.getElementById("blog-uc").style.display="none";
+    }, false);
+
+    document.querySelector("#forum").addEventListener('click', event => {
+        event.preventDefault();
+
+       document.getElementById("containers").style.display="none";
+       document.getElementById("poll").style.display="none";
+       document.getElementById("ads").style.display="none";
+       document.getElementById("upload-conteiner").style.display="none";
+       document.getElementById("forum-uc").style.display="block";
+       document.getElementById("blog-uc").style.display="none";
+    }, false);
+
+    document.querySelector("#blog").addEventListener('click', event => {
+        event.preventDefault();
+
+       document.getElementById("containers").style.display="none";
+       document.getElementById("poll").style.display="none";
+       document.getElementById("ads").style.display="none";
+       document.getElementById("upload-conteiner").style.display="none";
+       document.getElementById("forum-uc").style.display="none";
+       document.getElementById("blog-uc").style.display="block";
     }, false);
 
 }, false);
