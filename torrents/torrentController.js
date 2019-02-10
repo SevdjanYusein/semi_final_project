cdcbf3d4adba7139bd8e55cc9aa8b61ac49737a3
Sevdjan Yusein <sevdji_yusein@abv.bg>
2019-02-10 19:41:39 +0200

    torrents images and torrents view add

---
+++ torrents/torrentController.js
@@ -0,0 +1,37 @@
+function torrentsControler() {
+    torrents = torrentsStorage._allTorrents;
+
+    $.get('torrentTemplate.htm').then(text => {
+        const template = Handlebars.compile(text);
+
+        torrents.forEach(t => {
+            const html = template(t);
+            $('ul').append($(html));
+        });
+
+        $("#filterType").on("change", function (event) {
+            event.preventDefault();
+            // console.log("ti uspq ^^");
+            switch ($(this).val()) {
+                // case 'genre': torrents.sortByGenre(); break;
+                // case 'type': torrents.sortByType(); break;
+                case 'likes': {
+                    $.get('torrentTemplate.htm').then(text => {
+                        const template = Handlebars.compile(text);
+                        torrents.sortByLikes().forEach(t => {
+                            const html = template(t);
+                            $('ul').append($(html));
+                        });
+                    });
+                    break;
+                }
+                // case 'downloads': torrents.sortByDownloads(); break;
+            }
+        });
+
+
+        
+    });
+}
+
+torrentsControler();
