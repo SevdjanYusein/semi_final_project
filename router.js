6427c44a1ff2c0fdb753210e5c36f08023fb60f5
Sevdjan Yusein <sevdji_yusein@abv.bg>
2019-02-12 00:47:22 +0200

    added css in login form

--- router.js
+++ router.js
@@ -5,16 +5,27 @@ function router() {
         case 'home' : homeController(); break;
         case 'torrents' : torrentControler(); break;
         case 'catalogs' : catalogController(); break;
-        case 'upload' : uploadController(); break;
+        case 'upload' : checkIfIsLogged(); break;
         case 'forum' : forumController(); break;
         case 'subs' : subsController(); break;
         case 'blog' : blogController(); break;
         case 'profile-nav' : loginController(); break;
 
         default:
+            // homeController();
             break;
     }
 }
 
 $(window).on('hashchange', router);
 router();
+
+function checkIfIsLogged () {
+    var loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+    if (loggedUser) {
+        uploadController();
+    } else {
+        alert('Необходимо е да влезете в профила си!');
+        location.replace('#page=profile-nav');
+    }
+}
--- torrents/torrentController.js
+++ torrents/torrentController.js
@@ -13,9 +13,7 @@ function torrentControler() {
             $('#torrentsList').append($(html));
         });
 
-        $('#det').on('click', function(event) {
-            event.preventDefault();
-            console.log('clikam linka')
+        $('a').on('click', function(event) {
             $.get('catalogs/torrentDetails.htm').then(data => {
                 const template = Handlebars.compile(data);
 
@@ -26,8 +24,32 @@ function torrentControler() {
                         var currentTorrent = t;
                         const html = template(t);
                         $('main').html(html);
-                    }
+                    } 
                 }
+
+                var loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+                $('#just-like-it').on('click', function () {
+                    if (loggedUser) {
+                        currentTorrent.likes++;
+                        $('#haresvaniq').text('Харесвания: ' + currentTorrent.likes);
+                    } else {
+                        alert('Необходимо е да влезете в профила си!');
+                        location.replace('#page=profile-nav');
+                    }
+                });
+                $('#just-download-it').on('click', function () {
+                    if (loggedUser) {
+                        currentTorrent.downloads++;
+                        alert('Вие изтеглихе торента: ' + currentTorrent.name);
+                        $('#svalqniq').text('Сваляния: ' + currentTorrent.downloads);
+                        loggedUser.downloads++;
+                        localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+                        $('#downs').text('Свалени: ' + loggedUser.downloads);
+                    } else {
+                        alert('Необходимо е да влезете в профила си!');
+                        location.replace('#page=profile-nav');
+                    }
+                });
             });
         });
 
@@ -90,23 +112,47 @@ function showFilteredTorrents() {
             $('#torrentsList').append($(html));
         });
 
-        // $('#det').on('click', function(event) {
-        //     event.preventDefault();
-        //     console.log('clikam linka')
-        //     $.get('catalogs/torrentDetails.htm').then(data => {
-        //         const template = Handlebars.compile(data);
+        $('a').on('click', showDetails); 
 
-        //         let id = +location.hash.split('#')[1];
+    });
+}
 
-        //         for (let t of torrents) {
-        //             if (t.id === id) {
-        //                 var currentTorrent = t;
-        //                 const html = template(t);
-        //                 $('main').html(html);
-        //             }
-        //         }
-        //     });
-        // });
+function showDetails() {
+    $.get('catalogs/torrentDetails.htm').then(data => {
+        const template = Handlebars.compile(data);
 
+        let id = +location.hash.split('#')[1];
+
+        for (let t of torrents) {
+            if (t.id === id) {
+                var currentTorrent = t;
+                const html = template(t);
+                $('main').html(html);
+            } 
+        }
+
+        var loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+        $('#just-like-it').on('click', function () {
+            if (loggedUser) {
+                currentTorrent.likes++;
+                $('#haresvaniq').text('Харесвания: ' + currentTorrent.likes);
+            } else {
+                alert('Необходимо е да влезете в профила си!');
+                location.replace('#page=profile-nav');
+            }
+        });
+        $('#just-download-it').on('click', function () {
+            if (loggedUser) {
+                currentTorrent.downloads++;
+                alert('Вие изтеглихе торента: ' + currentTorrent.name);
+                $('#svalqniq').text('Сваляния: ' + currentTorrent.downloads);
+                loggedUser.downloads++;
+                localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+                $('#downs').text('Свалени: ' + loggedUser.downloads);
+            } else {
+                alert('Необходимо е да влезете в профила си!');
+                location.replace('#page=profile-nav');
+            }
+        });
     });
 }
